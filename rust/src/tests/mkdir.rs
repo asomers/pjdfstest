7a977ef42871c588f7ad8df439b95534b331ece1
Alan Somers <asomers@gmail.com>
2024-03-02 17:21:39 -0700

    Convert mkdir/05.t

--- rust/src/tests/mkdir.rs
+++ rust/src/tests/mkdir.rs
@@ -4,6 +4,7 @@ use nix::{sys::stat::Mode, unistd::mkdir};
 
 use crate::context::{SerializedTestContext, TestContext};
 
+use super::errors::eacces::eacces_search_permission_denied_test_case;
 use super::errors::eexist::eexist_file_exists_test_case;
 use super::errors::efault::efault_path_test_case;
 use super::errors::eloop::eloop_comp_test_case;
@@ -61,6 +62,9 @@ enametoolong_path_test_case!(mkdir(~path, Mode::empty()));
 
 // mkdir/04.t
 enoent_comp_test_case!(mkdir(~path, Mode::empty()));
+
+// mkdir/05.t
+eacces_search_permission_denied_test_case!(mkdir(~path, Mode::empty()));
 
 // mkdir/07.t
 eloop_comp_test_case!(mkdir(~path, Mode::empty()));
