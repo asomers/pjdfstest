06fecbcc411b962ecdf49014e205655c56bab949
Alan Somers <asomers@gmail.com>
2024-03-03 08:49:34 -0700

    Convert rename/05.t

--- rust/src/tests/rename.rs
+++ rust/src/tests/rename.rs
@@ -15,7 +15,10 @@ use crate::{
 use super::{
     assert_ctime_changed,
     errors::{
-        eacces::eacces_search_permission_denied_test_case2,
+        eacces::{
+            eacces_parent_dir_unwritable_test_case2,
+            eacces_search_permission_denied_test_case2
+        },
         efault::efault_either_test_case,
         eloop::eloop_either_test_case,
         enametoolong::{enametoolong_either_comp_test_case, enametoolong_either_path_test_case},
@@ -303,6 +306,9 @@ enoent_either_named_file_test_case!(rename);
 
 // rename/04.t
 eacces_search_permission_denied_test_case2!(rename);
+
+// rename/05.t
+eacces_parent_dir_unwritable_test_case2!(rename);
 
 // reanme/11.t
 eloop_either_test_case!(rename);
